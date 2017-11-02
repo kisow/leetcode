e0e14e7b2be85cbd12cb369e4e00bdd2d42b0605
Jaehoon Kim <kisow80@gmail.com>
2017-11-03 04:43:51 +0900

    406: optimize running time to 69ms

--- 406.cpp
+++ 406.cpp
@@ -12,51 +12,37 @@ ostream& print(auto& p) {
 ////////////////////////////////////////////////////////////////////////////////
 class Solution {
   public:
-    struct Meta {
-      int h_;
-      int idx_;
-      int current_k_;
-      Meta() {}
-      bool operator<(auto& o) const {
-        if(current_k_ == o.current_k_) return h_ > o.h_;
-        else return current_k_ > o.current_k_; 
-      }
-    };
-
+    vector<int> cks_;
     vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
-      vector<Meta> metas(people.size());
+      cks_.resize(people.size());
+
+      auto cmp = [&](auto& x, auto& y) {
+        if(cks_[x] == cks_[y]) return people[x].first > people[y].first;
+        else return cks_[x] > cks_[y]; 
+      };
 
       int i = 0;
       int min = 0;
       for(auto& p : people) {
-        metas[i].idx_ = i;
-        metas[i].h_ = p.first;
-        metas[i].current_k_ = p.second;
-        if(metas[min] < metas[i]) min = i;
+        cks_[i] = p.second;
+        if(cmp(min, i)) min = i;
         ++i;
       }
 
-      vector<pair<int, int>> out(people.size());
+      for(i = 0; i < people.size(); i++) {
+        auto p = people[min];
+        swap(cks_[i], cks_[min]);
+        swap(people[i], people[min]);
 
-      for(i = 0; not metas.empty(); i++) {
-        auto p = people[metas[min].idx_];
-        swap(metas[min], metas.back());
-        out[i] = p;
-        metas.pop_back();
-
-        min = 0;
-        int j = 0;
-        for(auto& meta : metas) {
-          auto& o = people[meta.idx_];
-          if(p.first >= o.first) {
-            meta.current_k_--;
-          }
-          if(metas[min] < meta) min = j;
-          j++;
+        min = i + 1;
+        for(int j = i + 1; j < cks_.size(); j++) {
+          auto& o = people[j];
+          if(p.first >= o.first) cks_[j]--;
+          if(cmp(min, j)) min = j;
         }
       }
 
-      return move(out);
+      return move(people);
     }
 };
 ////////////////////////////////////////////////////////////////////////////////
